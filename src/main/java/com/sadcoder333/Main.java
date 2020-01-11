e5af35574218f8943cabc1fbfe562c9fec67a3f5
sadcoder333 <yudeshui333@163.com>
2020-01-11 21:43:50 +0800

    update jar and main method

--- src/main/java/com/sadcoder333/Main.java
+++ src/main/java/com/sadcoder333/Main.java
@@ -2,10 +2,12 @@ package com.sadcoder333;
 
 import com.sadcoder333.config.DefaultImageConfig;
 import com.sadcoder333.config.ImageConfig;
-import com.sadcoder333.core.DownloadMode;
+import com.sadcoder333.core.DownloadConstants;
 import com.sadcoder333.core.ImageDownloadManager;
+import org.apache.commons.io.FileUtils;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,51 +16,54 @@ public class Main {
     /**
      * 从命令行执行程序时需要传递3个参数：
      * <p>
-     * 第一个参数表示下载模式，如果需要从html网页自动抓取图片下载这个参数为0， 如果需要直接通过图片链接下载这个参数为1
-     * 第二个参数表示图片下载目录，支持多级目录，如果目录不存在会自动创建子目录
-     * 当第一个参数为0时，第三个参数为网页地址，比如参数设置为http://abc/12345.html
-     * 当第一个参数为1时，第三个参数为多张图片地址列表，用英文逗号分隔多个参数，如http//abc/1.jpg,http://abc2.jpg
+     * 第一个参数表示图片下载目录，支持多级目录，如果目录不存在会自动创建子目录
+     * 第二个参数为htmls.txt时，必须存在这个文件，文件内容每行为html网页地址
+     * 第二个参数为images.txt时，必须存在这个文件，文件内容每行为每张图片的链接地址
      * <p>
      * <p>
      * <p>
-     * 抓取某个html网页的命令行如下：
-     * java -jar ImageDownloader-1.0.jar 0 D:/download http://abc/12345.html
+     * 抓取html网页中的图片的命令行如下：
+     * java -jar ImageDownloader-1.0.jar D:/download htmls.txt
      * <p>
      * 下载多张图片的命令行如下：
-     * java -jar ImageDownloader-1.0.jar 1 D:/download http//abc/1.jpg,http://abc2.jpg,http://abc2.jpg
+     * java -jar ImageDownloader-1.0.jar D:/download images.txt
      *
      * @param args 命令行参数
      * @throws Exception 异常
      */
     public static void main(String[] args) throws Exception {
-        if (args == null || args.length != 3) {
-            throw new IllegalArgumentException("please pass three arguments! ");
+        if (args == null || args.length != 2) {
+            throw new IllegalArgumentException("please pass one argument at least! ");
         }
 
-        int downloadMode;
-        try {
-            downloadMode = Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("The first argument should be an integer number");
-        }
-        if (downloadMode == DownloadMode.HTML.ordinal()) {
-            String downloadDir = args[1];
-            String htmlUrl = args[2];
+        String downloadDir = args[0];
+        String filename = args[1].trim();
+        if (DownloadConstants.FILE_HTMLS.equals(filename)) {
+            File htmlFile = new File(DownloadConstants.FILE_HTMLS);
+            List<String> htmlUrls;
+            try {
+                htmlUrls = FileUtils.readLines(htmlFile, "UTF-8");
+            } catch (IOException e) {
+                throw new IllegalArgumentException(e);
+            }
+            if (htmlUrls == null || htmlUrls.isEmpty()) {
+                throw new IllegalArgumentException("can not find htmls from " + DownloadConstants.FILE_HTMLS);
+            }
+
             ImageConfig imageConfig = new DefaultImageConfig.Builder()
                     .downloadDir(downloadDir)
                     .create();
-            ImageDownloadManager
-                    .getInstance(imageConfig)
-                    .downloadImages(htmlUrl);
-        } else if (downloadMode == DownloadMode.IMAGES.ordinal()) {
-            String downloadDir = args[1];
-            String arg2 = args[2];
-            List<String> imageUrls = Arrays
-                    .stream(arg2.split(","))
-                    .filter(url -> {
-                        String lowerUrl = url.toLowerCase();
-                        return lowerUrl.startsWith("http") || lowerUrl.startsWith("https");
-                    })
+
+            for (String htmlUrl : htmlUrls) {
+                ImageDownloadManager
+                        .getInstance(imageConfig)
+                        .downloadImages(htmlUrl);
+            }
+        } else if (DownloadConstants.FILE_IMAGES.equals(filename)) {
+            File imagesFile = new File(DownloadConstants.FILE_IMAGES);
+            List<String> imageUrls = FileUtils.readLines(imagesFile, "UTF-8")
+                    .stream()
+                    .filter(imageUrl -> imageUrl.toLowerCase().startsWith("http"))
                     .collect(Collectors.toList());
             if (imageUrls.isEmpty()) {
                 throw new IllegalArgumentException("Did you pass correct image urls?");
@@ -71,7 +76,7 @@ public class Main {
                     .getInstance(imageConfig)
                     .downloadImagesByUrl(imageUrls);
         } else {
-            throw new IllegalArgumentException("The first argument should be 0 or 1");
+            throw new IllegalArgumentException("can not find " + DownloadConstants.FILE_HTMLS + " or " + DownloadConstants.FILE_IMAGES);
         }
     }
 }
---
+++ src/main/java/com/sadcoder333/core/DownloadConstants.java
@@ -0,0 +1,8 @@
+package com.sadcoder333.core;
+
+public class DownloadConstants {
+
+    public static final String FILE_HTMLS = "htmls.txt";
+
+    public static final String FILE_IMAGES = "images.txt";
+}
